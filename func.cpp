aa00b2e46836dbe441b32b4afd4d07be7828235b
Simo64Terminator <simo64.tomasella@gmail.com>
2024-04-19 21:33:01 +0200

    Kept working on converting instructions

--- func.cpp
+++ func.cpp
@@ -10,7 +10,11 @@ void instructions() {
 
     if (choice == "NO") {
         std::cout << "This program simulates a trip over the Oregon Trail from\nIndependence, Missouri to Oregon City, Oregon in 1847.\n";
-        std::cout << "Your family of five will cover the 2040 mile Oregon Trail\n in 5-6 months --- if you make it alive.\n";
+        std::cout << "Your family of five will cover the 2040 mile Oregon Trail\n in 5-6 months --- if you make it alive.\n\n";
+        std::cout << "You had saved $900 to spend for the trip, and you've just paid $200 for a wagon.\n";
+        std::cout << "You will need to spend the rest of yur money on the following items:\n\n";
+        std::cout << "\tOxen - You can spend $200-$300 on your team\n";
+        std::cout << "\t\tThe more you spend, the faster you'll go because you'll have better animals\n\n";
     } 
 }
 
